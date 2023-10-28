7cb9ad8fe888991fbc97b0df25f0b516459a6ee7
Yerson Fabian Galvis <yersongalvis03@gmail.com>
2023-10-27 22:34:38 -0500

    Exercise answer

---
+++ TimeConversor/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace TimeConversor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {         
+            Console.WriteLine(TimerConversion.timeConversion("12:45:54PM"));
+        }
+
+        public class TimerConversion
+        {
+            public static string timeConversion(string s)
+            {
+                char delimiter = ':';
+                string[] Hora = s.Split(delimiter);
+
+                int tiempo = int.Parse(Hora[0]);
+                string UltimoTramo = Hora[2];
+                string UltimoTramosinHorario = UltimoTramo[^2..];
+
+
+                if (tiempo == 12 && UltimoTramosinHorario == "AM")
+                {
+                    Hora[0] = "00";
+                }
+                else if (tiempo != 12 && UltimoTramosinHorario == "PM" )
+                {
+                    tiempo += 12;
+                    Hora[0] = tiempo.ToString();
+                }
+                else if(tiempo == 12 && UltimoTramosinHorario == "PM")
+                {
+                    Hora[0] = Hora[0];
+                }
+
+                string HoraFinal = Hora[Hora.Length - 1].Replace("AM", "").Replace("PM", "");
+
+                return $"{Hora[0]}:{Hora[1]}:{HoraFinal}";
+            }
+        }
+
+
+    }
+}
